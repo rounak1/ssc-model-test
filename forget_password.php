8f90cfd06d8625e74cc6d4129517a9239b2433f5
Rounak <rounak1@gmail.com>
2022-06-06 18:28:23 +0600

    forget password template

--- forget_password.php
+++ forget_password.php
@@ -25,8 +25,179 @@ if (isset($_POST['forget_password'])) {
 
     $verifyEmailLink = "https://service.prothomalo.com/modeltest/reset_password?token=" . $token;
 
-    $message = '<div><div>প্রিয় শিক্ষার্থী বন্ধু,  এসএসসি মডেল টেস্ট ২০২২ - এ তোমাকে স্বাগতম।
-    </div> <a href="' . $verifyEmailLink . '">এই লিংকে ক্লিক</a> করে তোমার পাসওয়ার্ডটি পুনরায় সেট করো।</div>';
+    $message = '<style type="text/css">
+    body {
+      margin: 0 auto;
+      padding: 0;
+    }
+
+    .footer__social ul li:not(:last-child) {
+      margin-right: 7px;
+    }
+    .footer__social ul li {
+      display: inline-block;
+    }
+    .footer__widget ul li {
+      list-style: none;
+      line-height: 1;
+      margin-bottom: 12px;
+    }
+    .footer__social ul li a {
+      display: inline-block;
+      line-height: 40px;
+      text-align: center;
+    }
+
+    .footer__social ul li a:hover {
+      background-color: var(--tp-theme-1);
+      color: var(--tp-common-white);
+    }
+
+    @media only screen and (max-width: 660px) {
+      table[class="first_tbl"] {
+        width: 100% !important;
+        display: block !important;
+      }
+    }
+
+    @media only screen and (max-width: 510px) {
+      table[class="footer_first_item"] {
+        width: 100% !important;
+        display: block !important;
+      }
+    }
+  </style>
+
+
+
+  <table
+    width="660"
+    class="first_tbl"
+    border="0"
+    align="center"
+    cellspacing="0"
+    cellpadding="0"
+    bgcolor="#e5efe7"
+    style="
+      border-collapse: collapse;
+      font-family: Arial, Helvetica, sans-serif;
+      font-size: 12px;
+    "
+  >
+    <tr>
+      <td align="center" valign="top" style="padding: 16px 0">
+        <img
+          src="https://images.prothomalo.com/prothomalo-bangla/2022-06/e446f8f5-1f44-47b8-b595-4e7272371d31/porasona_removebg_preview.png"
+          height="36"
+          alt=""
+        />
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <table width="95%" align="center">
+          <tr>
+            <td>
+              <div
+                style="background-color: #fff; padding: 3%; margin-top: 3px"
+              >
+                <p
+                  style="
+                    font-size: 18px;
+                    font-weight: normal;
+                    line-height: 1.6;
+                    color: #333;
+                  "
+                >
+                  প্রিয় শিক্ষার্থী বন্ধু,
+                  <br />
+                  এসএসসি মডেল টেস্ট ২০২২ - এ তোমাকে স্বাগতম।
+                </p>
+
+                <p
+                  style="
+                    font-size: 18px;
+                    font-weight: normal;
+                    line-height: 1.6;
+                    color: #333;
+                  "
+                >
+                  <a href="' . $verifyEmailLink . '">এই লিংকে ক্লিক </a>করে
+                  তোমার পাসওয়ার্ডটি পুনরায় সেট করো।
+                </p>
+              </div>
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>
+        <table
+          width="95%"
+          border="0"
+          align="center"
+          cellpadding="0"
+          cellspacing="0"
+        >
+          <tr>
+            <td>
+              <table
+                class="footer_first_item"
+                border="0"
+                cellspacing="0"
+                cellpadding="0"
+                align="left"
+              >
+                <tr>
+                  <td align="left" valign="top">
+                    <div
+                      class="footer__social footer__widget"
+                      style="padding: 5px 0 20px 6px"
+                    >
+                      <h4
+                        style="
+                          font-weight: bold;
+                          font-family: Arial, Helvetica, sans-serif;
+                          font-size: 18px;
+                          padding: 0;
+                          margin: 0;
+                          padding: 0px 0px 0px 0px;
+                          display: inline-block;
+                        "
+                      >
+                        আমাদের সাথে যোগাযোগ:
+                      </h4>
+
+                      <a
+                        href="https://www.facebook.com/prothomalo.porasona/"
+                        target="_blank"
+                        style="margin-right: 6px"
+                        ><img
+                          src="https://images.prothomalo.com/prothomalo-bangla/2022-06/a51365e6-8135-41ef-b257-63fa3c74f55a/59439.png"
+                          alt=""
+                          height="22"
+                      /></a>
+
+                      <a href="mailto:porasona@prothomalo.com" target="_blank"
+                        ><img
+                          src="https://images.prothomalo.com/prothomalo-bangla/2022-06/9b794221-6c8b-4eb8-8087-57294873c2ea/er.png"
+                          alt=""
+                          height="25"
+                      /></a>
+                    </div>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>';
 
     if (!empty($email_address)) {
         //Store token in table
@@ -80,16 +251,16 @@ if (isset($_POST['forget_password'])) {
                   <div class="col-xxl-6 offset-xxl-3 col-xl-6 offset-xl-3 col-lg-8 offset-lg-2">
                      <div class="sign__wrapper white-bg">
                         <?php
-                          if (isset($_SESSION['success'])) {?>
+if (isset($_SESSION['success'])) {?>
                                 <div class="col-md-12 message-container">
                                   <div class="alert alert-success" role="alert">
                                     <?php echo $_SESSION['success']; ?>
                                   </div>
                                 </div>
-                          <?php 
-                            unset($_SESSION['success']);
-                            }
-                          ?>
+                          <?php
+unset($_SESSION['success']);
+}
+?>
                         <div class="sign__form">
                            <form action= "" method= "post">
                               <div class="sign__input-wrapper mb-25">
@@ -116,5 +287,5 @@ if (isset($_POST['forget_password'])) {
       </main>
 
 <?php
-    require 'footer-v2.php';
+require 'footer-v2.php';
 ?>
